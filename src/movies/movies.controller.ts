6dce1c0cc18aaa6049fbde5b5cf70c02f9eef419
앵가리 <>
2023-02-21 18:08:04 +0900

    Controller Decorator 활용

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+    @Get()
+    getAll() {
+        return 'This will return all movies';
+    }
+
+    @Get('/search')
+    search(@Query('year') movieData: string) {
+        return `searching for a movie made after: ${movieData}`
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') movieId: string) {
+        return `This will return one movie: ${movieId}`;
+    }
+
+    @Post()
+    create(@Body() movieData) {
+        return `This will create a movies: ${movieData.name}/${movieData.director}`;
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId: string, @Body() movieDate) {
+        return {
+            movieNumber: movieId,
+            ...movieDate
+        };
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId: string) {
+        return `This will remove a movie: ${movieId}`;
+    }
+}
