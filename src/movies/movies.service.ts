38263fc28a21347edd68470998ae7ee32e61b8fd
앵가리 <>
2023-02-21 19:14:46 +0900

    Service 로직 작성

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -4,4 +4,24 @@ import { Movie } from './entities/movie.entity';
 @Injectable()
 export class MoviesService {
     private movies: Movie[] = [];
+
+    getAll(): Movie[] {
+        return this.movies;
+    }
+
+    getOne(id: string): Movie {
+        return this.movies.find(movie => movie.id === +id) // +: parseInt()
+    }
+
+    remove(id: string): Boolean {
+        this.movies.filter(movie => movie.id !== +id);
+        return true;
+    }
+
+    create(movieData) {
+        this.movies.push({
+            id: this.movies.length + 1,
+            ...movieData
+        });
+    }
 }
