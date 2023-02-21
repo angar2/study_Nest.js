e78019af8e5272bb9c664a7bf59a299529b6f86e
앵가리 <>
2023-02-21 19:23:51 +0900

    Error 처리: NotFoundException()

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,10 +10,15 @@ export class MoviesService {
     }
 
     getOne(id: string): Movie {
-        return this.movies.find(movie => movie.id === +id) // +: parseInt()
+        const movie = this.movies.find(movie => movie.id === +id); // +: parseInt()
+        if(!movie) {
+            throw new NotFoundException(`Movie with ID ${id} not found.`);
+        }
+        return movie;
     }
 
     remove(id: string): Boolean {
+        this.getOne(id)
         this.movies.filter(movie => movie.id !== +id);
         return true;
     }
